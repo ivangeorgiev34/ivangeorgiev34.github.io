52280573b5b497ec1d24bccdc2bfaa1110de3b7c
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-10-03 23:13:17 +0300

    Change postcss-loader to scss files

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,19 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: ["tailwindcss", "autoprefixer"],
+              },
+            },
+          },
+          "sass-loader",
+        ],
       },
     ],
   },
